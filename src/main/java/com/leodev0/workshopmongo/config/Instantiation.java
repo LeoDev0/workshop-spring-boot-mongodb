9e611f190ffb24aa1a0da41f6b776ce1a9467e5c
Leonardo T. Fernandes <leo.80@protonmail.com>
2022-04-12 20:07:50 -0300

    chore: add instantiation of mass data for testing purposes

---
+++ src/main/java/com/leodev0/workshopmongo/config/Instantiation.java
@@ -0,0 +1,30 @@
+package com.leodev0.workshopmongo.config;
+
+import com.leodev0.workshopmongo.domain.User;
+import com.leodev0.workshopmongo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+    private final UserRepository userRepository;
+
+    public Instantiation(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        userRepository.deleteAll();
+
+        User maria = new User(null, "Maria Brown", "maria@gmail.com");
+        User alex = new User(null, "Alex Green", "alex@gmail.com");
+        User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+        userRepository.saveAll(Arrays.asList(maria, alex, bob));
+    }
+}
