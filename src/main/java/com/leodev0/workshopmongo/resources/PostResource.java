bd4492817e3d00b393985077300b3257fc145045
Leonardo T. Fernandes <leo.80@protonmail.com>
2022-04-16 19:48:45 -0300

    feat: add endpoint to get post by id

---
+++ src/main/java/com/leodev0/workshopmongo/resources/PostResource.java
@@ -0,0 +1,26 @@
+package com.leodev0.workshopmongo.resources;
+
+import com.leodev0.workshopmongo.domain.Post;
+import com.leodev0.workshopmongo.services.PostService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource {
+
+    private final PostService postService;
+
+    public PostResource(PostService postService) {
+        this.postService = postService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Post> findById(@PathVariable String id) {
+        Post post = postService.findById(id);
+        return ResponseEntity.ok().body(post);
+    }
+}
---
+++ src/main/java/com/leodev0/workshopmongo/services/PostService.java
@@ -0,0 +1,26 @@
+package com.leodev0.workshopmongo.services;
+
+import com.leodev0.workshopmongo.domain.Post;
+import com.leodev0.workshopmongo.repository.PostRepository;
+import com.leodev0.workshopmongo.services.exception.ObjectNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    private final PostRepository postRepository;
+
+    public PostService(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    public Post findById(String id) {
+        Optional<Post> post = postRepository.findById(id);
+        if (post.isEmpty()) {
+            throw new ObjectNotFoundException("Post of id " + id + " not found");
+        }
+        return post.get();
+    }
+}
